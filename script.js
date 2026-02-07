a5b5aa002832845eb9d5ff1a4065b7d43ad0fcc9
srishank-17 <keerthisrishank@gmail.com>
2026-02-07 14:12:49 +0530

    hello

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-// SLIDER
-let slides = document.querySelectorAll(".slide");
-let index = 0;
+// SCROLL NAVIGATION
+function scrollToSection(id){
+  document.getElementById(id).scrollIntoView({
+    behavior:'smooth'
+  });
+}
 
-document.getElementById("next").onclick = () => {
-  slides[index].classList.remove("active");
-  index = (index + 1) % slides.length;
-  slides[index].classList.add("active");
+
+// FOOD DATABASE
+const foods = {
+veg:[
+{name:"Paneer Curry",price:120,img:"https://via.placeholder.com/150"},
+{name:"Veg Biryani",price:100,img:"https://via.placeholder.com/150"}
+],
+
+nonveg:[
+{name:"Chicken Biryani",price:150,img:"https://via.placeholder.com/150"},
+{name:"Fish Fry",price:180,img:"https://via.placeholder.com/150"}
+],
+
+drinks:[
+{name:"Coke",price:40,img:"https://via.placeholder.com/150"},
+{name:"Coffee",price:30,img:"https://via.placeholder.com/150"}
+],
+
+dessert:[
+{name:"Ice Cream",price:70,img:"https://via.placeholder.com/150"},
+{name:"Cake",price:90,img:"https://via.placeholder.com/150"}
+]
 };
 
-document.getElementById("prev").onclick = () => {
-  slides[index].classList.remove("active");
-  index = (index - 1 + slides.length) % slides.length;
-  slides[index].classList.add("active");
-};
+
+// SHOW MENU ITEMS
+function showCategory(cat){
+const container=document.getElementById("foodContainer");
+container.innerHTML="";
+
+foods[cat].forEach(item=>{
+const card=document.createElement("div");
+card.className="card";
+
+card.innerHTML=`
+<img src="${item.img}">
+<h4>${item.name}</h4>
+<p>₹ ${item.price}</p>
+<button onclick="addToCart(${item.price})">Add</button>
+`;
+
+container.appendChild(card);
+});
+}
+
 
 // CART
-let total = 0;
+let total=0;
 
 function addToCart(price){
-  total += price;
-  document.getElementById("total").innerText = total;
+total+=price;
+document.getElementById("total").innerText=total;
 }
